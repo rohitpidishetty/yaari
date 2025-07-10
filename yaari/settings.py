e555fe9209c313f32078a793aed14914877eff28
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-07-10 01:24:21 -0500

    Update settings.py

--- yaari/settings.py
+++ yaari/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = ["https://yaari-jud.web.app", 
-                        "https://yaari-dba.web.app"
+                        "https://yaari-dba.web.app",
+                        "http://localhost:3000/"
                        ]
 
 CORS_ALLOW_METHODS = [
