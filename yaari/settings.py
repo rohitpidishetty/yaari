68d71b7d691822e38d344c23e0c86af4d266132c
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-11-22 19:58:49 -0600

    Update settings.py

--- yaari/settings.py
+++ yaari/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+import os
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -153,3 +153,4 @@ AWS_S3_REGION_NAME = 'us-east-2'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+
